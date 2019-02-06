c175c1d92a374e44efce2ffff5da0a925e1354b9
root <root@DESKTOP-UDLQTHR.localdomain>
2019-02-06 11:35:00 +0100

    sachakir.lua renvoit le pipeline et le tag est #lienFamille

--- sachakir.lua
+++ sachakir.lua
@@ -1,7 +1,6 @@
 local es = require("entree_sortie")
 local model = dark.model("postag-fr")
 
-
 function have_tag(seq, tag)
     return #seq[tag] ~= 0
 end
@@ -12,12 +11,10 @@ function string_tag(seq, tag)
     end
     local pos = seq[tag][1]
     local deb, fin = pos[1], pos[2]
-    
     local res = {}
     for i = deb,fin do
         res[#res+1] = seq[i].token
     end
-    
     return table.concat(res, " ")
 end
 
@@ -30,81 +27,50 @@ function alltag(seq, tag)
 	for i = 1,nombre_de_phrases do
 		local pos = seq[tag][i]
     	local deb, fin = pos[1], pos[2]
-
-    	--
     	restmp = {}
     	for j = deb,fin do
         restmp[#restmp+1] = seq[j].token
     	end
-    
     	phrase = table.concat(restmp, " ")
-    	--
 		res[#res+1] = phrase
 	end
 	return res
-
 end
 
 local P = dark.pipeline()
 P:basic()
-P:lexicon("#personnage", { "Donkey Kong", "Mario", "Pauline","Luigi","Yoshi" })
-P:lexicon("#famille", { "père", "mère", "frère","soeur","famille","parents","bébé","Bébé" })
+P:lexicon("#personnage", { "Donkey Kong", "Mario", "Pauline","Luigi","Yoshi" })--TODO a ajouter plus de mots
+P:lexicon("#famille", { "pere", "mere", "frere","soeur","famille","parents","bebe","Bebe" })--TODO a ajouter plus de mots
 
-local regle_phrase = dark.pattern([[
+P:pattern([[
     [#phrase
        	.+? ("." | "?" | "!" | "...")
     ]
 ]])
 
-local regle_4 = dark.pattern([[
-    [#idee
-       	"Mario" .*? "."
-    ]
-]])
-
-local regle_detecte_perso = dark.pattern([[
-    [#persoDetecte
-       	#POS=NNP
-    ]
-]])
-
-local regle_lien_famille = dark.pattern([[
+P:pattern([[
     [#family
        	(.* [#lienFamille #personnage ] .* #famille .* [#lienFamille #personnage ] .* ) | (.* #famille .* [#lienFamille #personnage ] .* [#lienFamille #personnage ] .* ) | (.* [#lienFamille #personnage ] .* [#lienFamille #personnage ] .* famille .* )
     ]
 ]])
 
 local taps = {
-    --["#personnage"] = "red",
-    --["#persoDetecte"] = "blue",
-    ["#family"] = "green",
-    ["#lienFamille"] = "yellow"
+    ["#personnage"] = "red",
+    ["#lienFamille"] = "blue"
 }
 
-texte = es.obtenir_les_lignes_de("Mario")
-
-text = texte[15]
+text = es.obtenir_les_lignes_de("Mario")
 text = text:gsub("%p"," %0 ")--Separation des virgules et des points...
-
-
 
 local seq = dark.sequence(text)
 P(seq)
-
-regle_phrase(seq)
-local phrases = alltag(seq, "#phrase")
+local phrases = alltag(seq, "#phrase")--phrases est la liste des phrases du texte
 for i, phrase in ipairs(phrases) do
 	local seq2 = dark.sequence(phrase)
 	P(seq2)
-	model(seq2)
-	regle_4(seq2)
-	regle_detecte_perso(seq2)
-	regle_lien_famille(seq2)
-	print("")
-	print(seq2:tostring(taps))
 end
-print("")
-print(seq:tostring())
 
-
-
+local M = {}
+M.regles = P
+return M
+--Tag : #lienFamille
