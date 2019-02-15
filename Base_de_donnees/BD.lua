4659a251a4c14b17a5a8fac2c1dcc918357b8fc2
Hakkerchief <haku_den@yahoo.fr>
2019-02-15 11:49:46 +0100

    inclusion du physique dans la base de donnees

--- Base_de_donnees/BD.lua
+++ Base_de_donnees/BD.lua
@@ -1,182 +1,324 @@
 return {
-  Samus = {
-    date = "1986",
-    cameo = "Metroid",
-    ["premiere_apparition"] = "NES Metroid",
-    serie = "Metroid",
-  },
-  Fox = {
-    date = "1993",
-    ["premiere_apparition"] = "Star Fox",
-    createur = "Shigeru Miyamoto",
-    cameo = "Super Smash Bros",
-    serie = "Star Fox",
-  },
-  Marth = {
-    date = "1990",
-    cameo = "Super Smash Bros",
-    ["premiere_apparition"] = "Fire Emblem",
-    serie = "Fire Emblem",
-  },
-  Mewtwo = {
-    createur = "Ken Sugimori",
-    cameo = "Pokemon Red and Blue",
-  },
-  Mario = {
-    date = "1981",
-    ["premiere_apparition"] = "Donkey Kong",
-    createur = "Shigeru Miyamoto",
-    cameo = "Donkey Kong",
-    serie = "Mario",
-  },
-  Isabelle = {
-    date = "2012",
-    cameo = "Super Smash Bros",
-    ["premiere_apparition"] = "Animal Crossing",
-  },
-  Luigi = {
-    createur = "Shigeru Miyamoto",
-    cameo = "Mario Bros",
-    ["premiere_apparition"] = "Mario Bros",
-    date = "1983",
-  },
-  Ganon = {
-    createur = "Shigeru Miyamoto",
-    cameo = "The Wind Waker",
-    ["premiere_apparition"] = "The Legend Of Zelda",
-    date = "1986",
-  },
-  Ridley = {
-    createur = "Makoto Kano",
-    cameo = "Super Smash Bros",
-    ["premiere_apparition"] = "Metroid",
-    date = "1986",
-  },
   Zelda = {
-    createur = "Shigeru Miyamoto",
-    cameo = "The Legend of Zelda du systeme",
     ["premiere_apparition"] = "The Legend",
+    createur = "Shigeru Miyamoto",
+    physique = {
+    },
+    cameo = "The Legend of Zelda du systeme",
     serie = "The Legend of Zelda",
   },
   Olimar = {
-    createur = "Shigeru Miyamoto",
-    cameo = "Nintendo",
     ["premiere_apparition"] = "Pikmin",
+    createur = "Shigeru Miyamoto",
+    physique = {
+    },
+    cameo = "Pikmin que pour Super Smash Bros",
     date = "2001",
-  },
-  Snake = {
-    createur = "Hideo Kojima",
-    cameo = "Scribblenauts",
-    ["premiere_apparition"] = "Metal Gear",
-    date = "1987",
-  },
-  Palutena = {
-    createur = "Gunpei Yokoi",
-    ["premiere_apparition"] = "Kid Icarus",
-    date = "1986",
-  },
-  Shulk = {
-    createur = "Tetsuya Takahashi",
-    ["premiere_apparition"] = "Xenoblade Chronicles",
-    date = "2010",
-  },
-  Rondoudou = {
-    date = "151",
-    ["premiere_apparition"] = "Pokemon Red",
-    createur = "Ken Sugimori",
-    cameo = "Pokemon Red and Blue",
-    serie = "Pokemon",
-  },
-  ["Donkey Kong"] = {
-    createur = "Shigeru Miyamoto",
-    cameo = "Donkey Kong",
-    ["premiere_apparition"] = "Donkey Kong",
-    date = "1981",
-  },
-  Link = {
-    createur = "Shigeru Miyamoto",
-    cameo = "Super Mario Bros",
-    date = "1987",
-    serie = "The Legend of Zelda",
-  },
-  Kirby = {
-    date = "1992",
-    ["premiere_apparition"] = "Game Boy Kirby",
-    createur = "Masahiro Sakurai",
-    cameo = "Super Smash Bros",
-    serie = "Kirby",
-  },
-  Yoshi = {
-    date = "1992",
-    ["premiere_apparition"] = "Super Mario World",
-    createur = "Kazumi Totaka",
-    cameo = "Super Mario World",
-    serie = "Smash",
-  },
-  Bowser = {
-    createur = "Shigeru Miyamoto",
-    cameo = "Super Mario Bros",
-    ["premiere_apparition"] = "Super Mario Bros",
-    date = "1985",
-  },
-  Felinferno = {
-    ["premiere_apparition"] = "VII",
-  },
-  Wario = {
-    date = "1992",
-    ["premiere_apparition"] = "Game Boy",
-    createur = "Hiroji Kiyotake",
-    cameo = "Game Boy",
-    serie = "Mario",
   },
   Cloud = {
     createur = "Tetsuya Nomura",
-    cameo = "Cloud",
-  },
-  Pichu = {
-    createur = "Pichu",
-    cameo = "Pokemon HeartGold",
-    ["premiere_apparition"] = "II",
+    physique = {
+    },
   },
   ["Captain Falcon"] = {
     createur = "Shigeru Miyamoto",
+    physique = {
+      habitPorte = {
+        [1] = "combinaison de vol bleue avec une armure corporelle typiquement avec son casque rouge emblematique avec une armure en forme de faucon dore sur son casque",
+        [2] = "gants jaunes",
+        [3] = "bottes",
+        [4] = "ceinture",
+      },
+    },
     ["premiere_apparition"] = "FZERO",
     date = "1991",
   },
-  Ness = {
-    date = "1994",
-    cameo = "Nintendo jeu",
-    ["premiere_apparition"] = "Earthbound",
-    serie = "EarthBound",
+  Ridley = {
+    ["premiere_apparition"] = "Metroid",
+    createur = "Makoto Kano",
+    physique = {
+    },
+    cameo = "Super Smash Bros",
+    date = "1986",
+  },
+  Snake = {
+    ["premiere_apparition"] = "Metal Gear",
+    createur = "Hideo Kojima",
+    physique = {
+    },
+    cameo = "Scribblenauts",
+    date = "1987",
+  },
+  ["Meta Knight"] = {
+    physique = {
+      habitPorte = {
+        [1] = "vetement de marque",
+        [2] = "cape bleu",
+        [3] = "gants",
+        [4] = "gants",
+      },
+    },
+    serie = "Kirby",
+    ["premiere_apparition"] = "Kirby",
+  },
+  Shulk = {
+    createur = "Tetsuya Takahashi",
+    physique = {
+    },
+    ["premiere_apparition"] = "Xenoblade Chronicles",
+    date = "2010",
+  },
+  Peach = {
+    createur = "Shigeru Miyamoto",
+    physique = {
+      habitPorte = {
+        [1] = "robe",
+      },
+    },
+    cameo = "Mario",
+    serie = "Mario",
+  },
+  Bowser = {
+    ["premiere_apparition"] = "Super Mario Bros",
+    createur = "Shigeru Miyamoto",
+    physique = {
+      habitPorte = {
+        [1] = "costume noir",
+        [2] = "cravate",
+      },
+    },
+    cameo = "Super Mario Bros",
+    date = "1985",
+  },
+  Link = {
+    physique = {
+      habitPorte = {
+        [1] = "bonnet vert",
+        [2] = "tunique verte",
+      },
+    },
+    cameo = "Super Mario Bros",
+    serie = "The Legend of Zelda",
+    date = "1987",
+  },
+  ["Donkey Kong"] = {
+    ["premiere_apparition"] = "Donkey Kong",
+    createur = "Shigeru Miyamoto",
+    physique = {
+      habitPorte = {
+        [1] = "cravate rouge",
+      },
+    },
+    cameo = "Donkey Kong",
+    date = "1981",
+  },
+  Samus = {
+    ["premiere_apparition"] = "NES Metroid",
+    physique = {
+      habitPorte = {
+        [1] = "combinaison de protection dans la plupart des series de Metroid",
+        [2] = "combinaison de protection construite par la race Chozo",
+        [3] = "costume bleu",
+      },
+    },
+    cameo = "Metroid",
+    serie = "Metroid",
+    date = "1986",
   },
   Pikachu = {
-    date = "1996",
+    physique = {
+    },
     ["premiere_apparition"] = "generation Red",
     createur = "Satoshi Tajiri",
     cameo = "Super Smash Bros",
     serie = "Pokemon",
+    date = "1996",
+  },
+  Pichu = {
+    physique = {
+    },
+    cameo = "Pokemon HeartGold",
+    ["premiere_apparition"] = "II",
+  },
+  Ganon = {
+    ["premiere_apparition"] = "The Legend Of Zelda",
+    createur = "Shigeru Miyamoto",
+    physique = {
+      habitPorte = {
+        [1] = "cape",
+        [2] = "armure",
+      },
+    },
+    cameo = "The Wind Waker",
+    date = "1986",
+  },
+  Marth = {
+    ["premiere_apparition"] = "Fire Emblem",
+    physique = {
+    },
+    cameo = "Super Smash Bros",
+    serie = "Fire Emblem",
+    date = "1990",
+  },
+  Mario = {
+    physique = {
+      habitPorte = {
+        [1] = "casquette",
+        [2] = "bonnet rouge",
+        [3] = "gants blancs",
+        [4] = "chemise rouge",
+        [5] = "combinaison bleue",
+        [6] = "chaussures marron",
+      },
+    },
+    createur = "Shigeru Miyamoto",
+    cameo = "Super Mario Bros",
+    serie = "Mario",
+    date = "1997",
+  },
+  Yoshi = {
+    physique = {
+    },
+    ["premiere_apparition"] = "Super Mario World",
+    createur = "Kazumi Totaka",
+    cameo = "Super Mario World",
+    serie = "Smash",
+    date = "1992",
+  },
+  Luigi = {
+    ["premiere_apparition"] = "Mario Bros",
+    createur = "Shigeru Miyamoto",
+    physique = {
+      habitPorte = {
+        [1] = "chaussures",
+        [2] = "combinaison verte",
+        [3] = "maillot vert",
+        [4] = "combinaison bleu",
+        [5] = "chemise verte",
+      },
+    },
+    cameo = "Mario Bros",
+    date = "1983",
   },
   Ike = {
+    physique = {
+    },
     cameo = "Super Smash Bros",
   },
-  Peach = {
-    createur = "Shigeru Miyamoto",
-    cameo = "Mario",
+  Ness = {
+    ["premiere_apparition"] = "Earthbound",
+    physique = {
+    },
+    cameo = "Nintendo jeu",
+    serie = "EarthBound",
+    date = "1994",
+  },
+  Mewtwo = {
+    createur = "Ken Sugimori",
+    physique = {
+    },
+    cameo = "Pokemon Red and Blue",
+  },
+  Isabelle = {
+    physique = {
+      habitPorte = {
+        [1] = "costume",
+      },
+    },
+    cameo = "Super Smash Bros",
+    ["premiere_apparition"] = "Animal Crossing",
+    date = "2012",
+  },
+  Kirby = {
+    physique = {
+    },
+    ["premiere_apparition"] = "Game Boy Kirby",
+    createur = "Masahiro Sakurai",
+    cameo = "Super Smash Bros",
+    serie = "Kirby",
+    date = "1992",
+  },
+  Felinferno = {
+    physique = {
+      habitPorte = {
+        [1] = "ceinture",
+        [2] = "chemise",
+      },
+    },
+    ["premiere_apparition"] = "VII",
+  },
+  Fox = {
+    ["premiere_apparition"] = "Star Fox",
+    physique = {
+    },
+    cameo = "Super Smash Bros",
+    serie = "Star Fox",
+    date = "1993",
+  },
+  Rondoudou = {
+    physique = {
+    },
+    ["premiere_apparition"] = "Pokemon Red",
+    createur = "Ken Sugimori",
+    cameo = "Pokemon Red and Blue",
+    serie = "Pokemon",
+    date = "151",
+  },
+  Palutena = {
+    createur = "Gunpei Yokoi",
+    physique = {
+      habitPorte = {
+        [1] = "robe bleue",
+        [2] = "robe blanche",
+      },
+    },
+    ["premiere_apparition"] = "Kid Icarus",
+    date = "1986",
+  },
+  Wario = {
+    physique = {
+      habitPorte = {
+        [1] = "tenue de plombier avec une palette de couleurs jaune",
+        [2] = "chemise jaune",
+        [3] = "combinaison violette",
+        [4] = "chaussures vertes",
+        [5] = "gants blancs",
+        [6] = "manteau",
+        [7] = "chemise bleu",
+        [8] = "ceinture rouge",
+        [9] = "chaussures bleues",
+        [10] = "gants",
+        [11] = "tenue de motard des jeux WarioWare",
+        [12] = "tenue de plombier similaire a celle de Mario",
+        [13] = "casquette jaune",
+        [14] = "gants blancs",
+        [15] = "casquette",
+        [16] = "vetement bleu",
+        [17] = "maillot",
+        [18] = "armure",
+      },
+    },
+    ["premiere_apparition"] = "Game Boy",
+    createur = "Hiroji Kiyotake",
+    cameo = "Game Boy",
     serie = "Mario",
+    date = "1992",
+  },
+  ["Samus sombre"] = {
+    ["premiere_apparition"] = "Metroid Prime",
+    physique = {
+      habitPorte = {
+        [1] = "costume",
+      },
+    },
+    cameo = "Super Smash Bros",
+    serie = "Metroid",
+    date = "2004",
   },
   Rosalina = {
     createur = "Shigeru Miyamoto",
+    physique = {
+    },
     cameo = "Mario Kart recemment",
-  },
-  ["Samus sombre"] = {
-    date = "2004",
-    cameo = "Super Smash Bros",
-    ["premiere_apparition"] = "Metroid Prime",
-    serie = "Metroid",
-  },
-  ["Meta Knight"] = {
-    date = "1993",
-    ["premiere_apparition"] = "Kirby",
-    serie = "Kirby",
   },
 }
--- processingData.lua
+++ processingData.lua
@@ -3,7 +3,7 @@ entree_sortie = require("entree_sortie")
 -- On recupere les pipelines
 pUnivers = require("patternUnivers")
 pCameoSerie = require("serieCameo")
-relations = require("relations")
+pPhysique = require("physique")
 
 local function have_tag(seq, tag)
     return #seq[tag] ~= 0
@@ -24,6 +24,26 @@ local function string_tag(seq, tag)
     return table.concat(res, " ")
 end
 
+function list_string_tag(seq, tag)
+    if not have_tag(seq, tag) then
+        return
+    end
+    local taille_tag_seq = #seq[tag]
+    local table_res = {}
+    for nString = 1,taille_tag_seq do 
+        local pos = seq[tag][nString]
+        local deb, fin = pos[1], pos[2]
+        
+        local res = {}
+        for i = deb,fin do
+            res[#res+1] = seq[i].token
+        end
+        table_res[#table_res+1] = table.concat(res, " ")
+    end
+    return table_res
+end
+
+
 local function enlever_accents(texte)
     texte = texte:gsub("ä", "a")
     texte = texte:gsub("â", "a")
@@ -41,7 +61,6 @@ local function enlever_accents(texte)
     texte = texte:gsub("ü", "u")
     texte = texte:gsub("û", "u")
     texte = texte:gsub("ù", "u")
-	texte = texte:gsub("'", " ")
     return texte
 end
 
@@ -51,13 +70,13 @@ local taps = {
     ["#fa"] = "green",
     ["#serie"] = "red",
     ["#appearance"] = "purple",
-    ["#lienFamille"] = "red"
+    ["#habitPorte"] = "cyan",
 }
 
 local data = {}
 local fichiers = obtenir_tous_les_textes()
+local test_nom = "Luigi"
 
----### Apply the patterns on every file ###
 for nom,texte in pairs(fichiers) do
     personage_tab = {}
     texte = texte:gsub("%p", " %0 ")
@@ -65,20 +84,20 @@ for nom,texte in pairs(fichiers) do
 	local seq = dark.sequence(texte)
 	pUnivers(seq)
     pCameoSerie(seq)
-    relations(seq)
-    personage_tab["lienFamille"] = string_tag(seq,"#lienF")
+    pPhysique(seq)
     personage_tab["createur"] = string_tag(seq, "#cre")
     personage_tab["date"] = string_tag(seq, "#date")
     personage_tab["premiere_apparition"] = string_tag(seq, "#fa")
     personage_tab["serie"] = string_tag(seq, "#serie")
     personage_tab["cameo"] = string_tag(seq, "#appearance")
+    personage_tab["physique"] = {}
+    personage_tab["physique"]["habitPorte"] = list_string_tag(seq, "#habitPorte")
     data[nom] = personage_tab
 end
 
 ecrire_dans_la_bd(data)
 
----### Test to display the analysis of one file ###
-local test_nom = "Captain Falcon"
+
 test = obtenir_les_lignes_de(test_nom)
 --print(test)
 test = test:gsub("%p", " %0 ")
@@ -86,7 +105,7 @@ test = enlever_accents(test)
 seq_test = dark.sequence(test)
 pUnivers(seq_test)
 pCameoSerie(seq_test)
-relations(seq_test)
---print(seq_test:tostring(taps))
+pPhysique(seq_test)
+print(seq_test:tostring(taps))
 
 
