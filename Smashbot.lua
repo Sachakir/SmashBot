5519a9069346a9585474e15a3ef0274c853666e1
Sachakir <sachakir1357@gmail.com>
2019-02-15 10:07:16 +0100

    Bails relations

--- Smashbot.lua
+++ Smashbot.lua
@@ -41,6 +41,12 @@ end
 local function obtenir_info_reponse(reponse, nom, info)
     res = data_traitement.obtenir_objet_de_personnage_par_clef(data,nom)
     res = data_traitement.obtenir_objet_de_personnage_par_clef(res, info)
+    if (type(res) == "table") then
+        str = ""
+        for i = 1, #res do
+            str = str .. res[i] .. ","
+        end
+    end
     return res
 end
 
@@ -99,7 +105,7 @@ local function preparation_reponse(reponse, memoire)
             return "Je ne sais pas."
         end
         return nom.." est vu pour la première fois dans "..info
-    end
+    
     elseif possede_tag(reponse, "#ami") then
         info = obtenir_info_reponse(reponse, nom, "ami")
         if info == nil then
